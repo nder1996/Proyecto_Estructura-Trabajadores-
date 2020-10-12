23e421bfc5846b142fd49017a8513138d3c482ee
Ander_AAM <72629491+nder1996@users.noreply.github.com>
2020-10-12 13:26:36 -0500

    V10.0

--- ProyectoModificable.py
+++ ProyectoModificable.py
@@ -57,22 +57,22 @@ def D_Comprobar(L_Data,Data,Bandera):
    return False
           
 def ValidadorC(Variable,Tipo):
-    Busq1 = re.search('[0-9]',Variable)
-    Busq2 = re.search("[A-Za-z]",Variable)
-    if Tipo==1:
-       if Busq1==None:
-        os.system ("cls")
-        print("----------------------------------------------------------------") 
-        print("Ingrese Solo Numeros [DEL 0 HASTA EL 9] Para Llenar Este Campo")
-        print("----------------------------------------------------------------\n") 
-        IngresarD()
-    if Tipo==0:
-        if Busq2==None:
-          os.system ("cls")
-          print("----------------------------------------------------------------") 
-          print("Ingrese Solo Letras [DE LA A HASTA EL Z] Para Llenar Este Campo")
-          print("----------------------------------------------------------------\n") 
-          IngresarD()
+ if Tipo==1:
+      if Variable.isdigit()==False:
+         os.system ("cls")
+         print("----------------------------------------------------------------") 
+         print("Ingrese Solo Numeros [DEL 0 HASTA EL 9] Para Llenar Este Campo")
+         print("----------------------------------------------------------------\n") 
+         IngresarD()
+ if Tipo==0:
+      i=0;  
+      for i in range(len(Variable)):
+         if re.search( "[A-Za-z]",Variable[i])==None:
+            os.system ("cls")
+            print("----------------------------------------------------------------") 
+            print("Ingrese Solo Letras [DE LA A HASTA EL Z] Para Llenar Este Campo")
+            print("----------------------------------------------------------------\n") 
+            IngresarD()
 
 def GuardarD(Nombre,Apellido,Cedula,Cedula_l,Ciudad_V,Lugar_V,Mesa_V,Puesto_V,DatosV,DatosL):
   Nombre=Nombre+"-";Apellido=Apellido+"-";Cedula=Cedula+"-";Cedula_l=Cedula_l+"-";Ciudad_V=Ciudad_V+"-";Lugar_V=Lugar_V+"-";Mesa_V=Mesa_V+"-";Puesto_V=Puesto_V+"-"
@@ -102,20 +102,18 @@ def IngresarD():
    Cedula = input("\nIngrese Su # Cedula  : ")
    Cedula = Cedula.replace(" ", "");ValidadorC(Cedula,1)
    if D_Comprobar(Votantes,Cedula,Bandera)==False:
-      Nombre  = input("\nIngrese Su Nombre    : ")
-      ValidadorC(Nombre,0);
-      Apellido= input("\nIngrese Su Apellido  : ")
-      ValidadorC(Apellido,0);
-      Cedula_l= input("\nIngrese El # Cedula De Su Lider : ")
-      Cedula_l= Cedula_l.replace(" ", "");ValidadorC(Cedula_l,1);
-      Ciudad_V = input("\nIngrese Su Ciudad Que Le Corresponde : ")
-      ValidadorC(Ciudad_V,0);
-      Lugar_V = input("\nIngrese Su Lugar De Votacion : ")
-      ValidadorC(Lugar_V,0);
-      Mesa_V = input("\nIngrese El # De la Mesa Correspondiente : ")
-      ValidadorC(Mesa_V,1);
-      Puesto_V = input("\nIngrese El # De la Mesa Correspondiente : ")
-      ValidadorC(Mesa_V,1);
+      Nombre    = input("\nIngrese Su Nombre    : ")
+      S_Espacio = Nombre.replace(" ", "");ValidadorC(S_Espacio,0);S_Espacio=""
+      Apellido  = input("\nIngrese Su Apellido  : ")
+      S_Espacio = Apellido.replace(" ", "");ValidadorC(S_Espacio,0);S_Espacio=""
+      Cedula_l  = input("\nIngrese El # Cedula De Su Lider : ")
+      Cedula_l  = Cedula_l.replace(" ", "");ValidadorC(Cedula_l,1);
+      Ciudad_V  = input("\nIngrese Su Ciudad Que Le Corresponde : ")
+      S_Espacio = Ciudad_V.replace(" ", "");ValidadorC(S_Espacio,0);
+      Lugar_V   = input("\nIngrese Su Lugar De Votacion : ")
+      S_Espacio = Lugar_V.replace(" ", "");ValidadorC(S_Espacio,0);
+      Mesa_V    = input("\nIngrese El # De la Mesa Correspondiente : ");ValidadorC(Mesa_V,1);
+      Puesto_V  = input("\nIngrese El # Del Puesto Correspondiente : ");ValidadorC(Puesto_V,1);
       GuardarD(Nombre,Apellido,Cedula,Cedula_l,Ciudad_V,Lugar_V,Mesa_V,Puesto_V,DatosV,DatosL)
    else:
       os.system ("cls")
@@ -125,15 +123,9 @@ def IngresarD():
       IngresarD()
    
 
-   
+
 InvBase()
 
 IngresarD()
-
-
-#MostrarD()+
-
-
-#IngresarD()
 
 
--- Validaciones.py
+++ Validaciones.py
@@ -1,1 +1,12 @@
+import string
+import re
+import os
 
+
+Num = "1234556";i=0
+
+#if Busq == re.search('[\d]',"1")!=None:
+#print("CORRECTO")
+
+ 
+print(Num.isdigit())
